4894ec3ba8f081b08334a092d8fef14de0ffcd62
Eric Hoover <ehoover@dminc.com>
2022-10-23 15:12:05 -0400

    added more functions to 8kyu.ts

--- 8kyu.ts
+++ 8kyu.ts
@@ -12,6 +12,10 @@ export class Kata {
 export const makeNumberNegative = (num: number): number => -Math.abs(num);
 
 // strings
+export function makeUpperCase(str: string): string {
+  return str.toUpperCase();
+}
+
 export function writeNumberToEvenOrOdd(n: number): string {
   return n % 2 == 0 ? "Even" : "Odd";
 }
@@ -25,12 +29,37 @@ export function repeatString(n: number, s: String): String {
 }
 
 export function removeFirstAndLastCharacterFromString(str: string): string {
-    return str.slice(1,-1);
-  }
+  return str.slice(1, -1);
+}
+
+export function convertNameToIntials(name: string): string {
+  return name
+    .toUpperCase()
+    .split(" ")
+    .map((word) => word[0])
+    .join(".");
+}
 
 // arrays
+export function findMinimumNumber(args: number[]): number {
+  return Math.min(...args);
+}
+
+export function doubleEachNumberInArray(x: number[]): number[] {
+  return x.map((number) => number * 2);
+}
+
 export function addOnlyPositiveNumbers(arr: number[]): number {
   return arr.reduce((sum, n) => (n > 0 ? sum + n : sum), 0);
+}
+
+export function sumSquareOfEachNumber(numbers: number[]): number {
+  return numbers.reduce((sum, current) => sum + current * current, 0);
+}
+
+export function findWordIndexInArray(haystack: any[]): string {
+  let index = haystack.indexOf("needle");
+  return `found the needle at position ${index}`;
 }
 
 // convert data types
@@ -38,6 +67,14 @@ export function convertNumberToString(num: number): string {
   return num.toString();
 }
 
-export const convertBoolToString = (bool: boolean): string => {
+export function convertStringToNumber(str: string): number {
+  return Number(str);
+}
+
+export const convertBoolToYesOrNo = (bool: boolean): string => {
   return bool ? "Yes" : "No";
+};
+
+export const booleanToString = (boolean: boolean): string => {
+  return boolean.toString();
 };
